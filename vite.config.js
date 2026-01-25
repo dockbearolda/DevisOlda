f88f9351104d9d69a3aed7b0914968692d4e6912
dockbearolda <dockbear@gmail.com>
2026-01-24 20:43:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-  publicDir: 'public',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        devis: resolve(__dirname, 'devis.html')
-      }
-    }
   }
 })
